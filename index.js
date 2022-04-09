e822c369a7ad4b3e7a928bfa1eedcfc47ac6dfa7
Maria T. Andersen <mteilmann93@gmail.com>
2022-04-09 15:21:41 +0200

    show the window by daytime in browser as default

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("Hello World");
+const windowFrame = document.getElementById("window-container");
+
+let windowByDaytime = document.createElement("img");
+windowByDaytime.src = "./img/day.PNG";
+windowByDaytime.alt = "Window by daytime";
+
+windowFrame.appendChild(windowByDaytime);
